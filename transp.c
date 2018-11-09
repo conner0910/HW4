1a70049139355941fdc4940ad19591875a8c768f
conner0910 <conner0910@gmail.com>
2018-11-09 18:32:33 -0500

    Added transp.c and transpif.c. Malloced the arrays in transp.c

---
+++ transp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv){
+  int N = atoi(argv[1]);
+  int block = atoi(argv[2]);  
+  float *input = malloc(N * N * sizeof(float));
+  float *output = malloc(N * N * sizeof(float));
+  float multiplier;
+  float randNum;
+  srand(time(NULL));
+  for (int i = 0; i < N * N; i++){
+    multiplier = rand() / (float) RAND_MAX;
+    randNum = -100 + multiplier *  (100 + 100);
+    input[i] = randNum;
+  }
+  
+  free(input);
+  free(output);
+}
